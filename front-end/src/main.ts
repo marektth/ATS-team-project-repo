12d317b33f70bffb1bb8ad51f8b13a7d3277d4b8
Filip Havel <filip.havel@student.tuke.sk>
2021-10-20 09:26:25 +0200

    recreated project and added visma nordic cool vue library

--- front-end/src/main.ts
+++ front-end/src/main.ts
@@ -1,5 +1,14 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import UI from '@vismaux/nordic-cool-vue';
+import '@vismaux/nordic-cool-vue/dist/nc.css';
 
-createApp(App).use(router).mount('#app')
+Vue.use(UI);
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
---
+++ front-end/src/shims-visma.d.ts
@@ -0,0 +1,1 @@
+declare module '@vismaux/nordic-cool-vue';
