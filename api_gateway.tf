02280c81b929358e6d05b04327358878aefcb76a
Marek Toth <marektoth199@gmail.com>
2021-10-25 21:47:35 +0200

    adding apigw access to lambda

--- api_gateway.tf
+++ api_gateway.tf
@@ -39,3 +39,7 @@ resource "aws_api_gateway_deployment" "example" {
   rest_api_id = "${aws_api_gateway_rest_api.example.id}"
   stage_name  = "test"
 }
+
+output "base_url" {
+  value = "${aws_api_gateway_deployment.example.invoke_url}"
+}
--- lambda.tf
+++ lambda.tf
@@ -73,3 +73,14 @@ resource "aws_api_gateway_method" "proxy" {
   http_method   = "ANY"
   authorization = "NONE"
 }
+
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.example.function_name}"
+  principal     = "apigateway.amazonaws.com"
+
+  # The /*/* portion grants access from any method on any resource
+  # within the API Gateway "REST API".
+  source_arn = "${aws_api_gateway_rest_api.example.execution_arn}/*/*"
+}
