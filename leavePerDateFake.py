e2dfd6a2a613da8758e7cfdeb25db92fd85497a8
TomasVank <72217029+TomasVank@users.noreply.github.com>
2021-10-29 00:18:57 +0200

    Update leavePerDateFake.py

--- leavePerDateFake.py
+++ leavePerDateFake.py
@@ -9,37 +9,33 @@ import numpy as np
 # %%
 df = pd.read_csv("database.csv")
 # %%
-print(df)
-# %%
 dfPerDays = pd.DataFrame()  
-HoursOfLeave = ("4", "8")
-# %%
-print(dfPerDays)
-
-# %%
 fake = Faker()
+HoursOfLeave = ("4:00", "4:30", "5:00", "5:30", "6:00", "6:30", "7:00", "7:30", "8:00")
 
 ListOfPersonNumber = []
 ListOfEmploymentNumber = []
 ListOfLeaveYear = []
 ListOfLeaveTypeCode = []
 ListOfHoursOfLeave = []
-
 ListOfSequenceNumbers = []
 ListOfDateOfLeave = []
 
+
+# %%
 for x in range(df.shape[0]):
     start = pd.to_datetime(df['StartDate'][x])
-    while str(start) != str((pd.to_datetime(df['EndDate'][x])  + timedelta(days=1)).date()):
+    hourse = random.choice(HoursOfLeave)
+    while str(start) != str((pd.to_datetime(df['EndDate'][x]) + timedelta(days=1)).date()):
         ListOfPersonNumber.append(df['PersonNumber'][x])
         ListOfEmploymentNumber.append(df['EmploymentNumber'][x])
-        ListOfLeaveYear.append(df['LeaveYear'][x].item())
+        ListOfLeaveYear.append(df['LeaveYear'][x])
         ListOfLeaveTypeCode.append(df['LeaveTypeCode'][x])
-
         ListOfSequenceNumbers.append(df['SequenceNumber'][x])
-        ListOfHoursOfLeave.append(random.choice(HoursOfLeave))
 
+        ListOfHoursOfLeave.append(hourse)
         ListOfDateOfLeave.append(start)
+
         start = (pd.to_datetime(start)  + timedelta(days=1)).date()
 
 #%%
@@ -47,22 +43,19 @@ dfPerDays["PersonNumber"] = ListOfPersonNumber
 dfPerDays["EmploymentNumber"] = ListOfEmploymentNumber
 dfPerDays["LeaveYear"] = ListOfLeaveYear
 dfPerDays["LeaveTypeCode"] = ListOfLeaveTypeCode
-
 dfPerDays["SequenceNumber"] = ListOfSequenceNumbers
 dfPerDays["HoursOfLeave"] = ListOfHoursOfLeave
-
 dfPerDays["DateOfLeave"] = ListOfDateOfLeave
 
 # %%
-print(dfPerDays)
-# %%
-dfPerDays['DateOfLeave'] = dfPerDays['DateOfLeave'].dt.strftime('%Y-%m-%d')
-for x in range(dfPerDays.shape[0]):   
-    print(type(dfPerDays["DateOfLeave"][x]))    
-
+dfPerDays["DateOfLeave"] = pd.to_datetime(dfPerDays["DateOfLeave"])
+dfPerDays['DateOfLeave'] = dfPerDays["DateOfLeave"].dt.strftime("%d/%m/%Y")
 #%%
-for x in range(dfPerDays.shape[0]):   
-    print(type(dfPerDays["LeaveYear"][x]))  
+for column in dfPerDays:
+    dfPerDays[column] = dfPerDays[column].apply(str) 
+
+# %%
+print(dfPerDays)
 # %%
 dfPerDays.to_csv("databasePerDay.csv")
 
