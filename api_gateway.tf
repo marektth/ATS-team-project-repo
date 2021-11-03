b7db5ae41ee72c6680eadc3b18bf7bdce6733ffb
Marek Toth <marektoth199@gmail.com>
2021-11-03 12:07:57 +0100

    testing new TF code v1.1

--- api_gateway.tf
+++ api_gateway.tf
@@ -5,8 +5,8 @@ resource "aws_api_gateway_rest_api" "example" {
 
 resource "aws_api_gateway_integration" "lambda" {
   rest_api_id = "${aws_api_gateway_rest_api.example.id}"
-  resource_id = "${aws_api_gateway_method.proxy.resource_id}"
-  http_method = "${aws_api_gateway_method.proxy.http_method}"
+  resource_id = "${aws_api_gateway_method.post.resource_id}"
+  http_method = "${aws_api_gateway_method.post.http_method}"
 
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
@@ -15,8 +15,8 @@ resource "aws_api_gateway_integration" "lambda" {
 
 resource "aws_api_gateway_integration" "lambda2" {
   rest_api_id = "${aws_api_gateway_rest_api.example.id}"
-  resource_id = "${aws_api_gateway_method.proxy.resource_id}"
-  http_method = "${aws_api_gateway_method.proxy.http_method}"
+  resource_id = "${aws_api_gateway_method.get.resource_id}"
+  http_method = "${aws_api_gateway_method.get.http_method}"
 
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
@@ -49,8 +49,8 @@ resource "aws_api_gateway_integration" "lambda_root" {
 
 resource "aws_api_gateway_integration" "lambda_root2" {
   rest_api_id = "${aws_api_gateway_rest_api.example.id}"
-  resource_id = "${aws_api_gateway_method.proxy_root.resource_id}"
-  http_method = "${aws_api_gateway_method.proxy_root.http_method}"
+  resource_id = "${aws_api_gateway_method.proxy_root2.resource_id}"
+  http_method = "${aws_api_gateway_method.proxy_root2.http_method}"
 
   integration_http_method = "GET"
   type                    = "AWS_PROXY"
