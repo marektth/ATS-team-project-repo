cedce2d7b687e662928b850f84b5f24df96ee8da
Marek Toth <marektoth199@gmail.com>
2021-11-03 12:30:49 +0100

    testing new TF code v 1.6

--- lambda.tf
+++ lambda.tf
@@ -111,15 +111,14 @@ resource "aws_lambda_permission" "apigw" {
   source_arn = "${aws_api_gateway_rest_api.example.execution_arn}/POST/submit"
 }
 
-#resource "aws_lambda_permission" "apigw2" {
-#  count = "${length(var.lambdas)}"
-  #name = "${element(var.lambdas,count.index )}"
-#  statement_id  = "AllowAPIGatewayInvoke"
-#  action        = "lambda:InvokeFunction"
-#  function_name = "${aws_lambda_function.post_lambda.function_name}"
-#  principal     = "apigateway.amazonaws.com"
+resource "aws_lambda_permission" "apigw2" {
+
+  statement_id  = "AllowAPIGatewayInvoke_GET"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.post_lambda.function_name}"
+  principal     = "apigateway.amazonaws.com"
 
   # The /*/* portion grants access from any method on any resource
   # within the API Gateway "REST API".
-#  source_arn = "${aws_api_gateway_rest_api.example.execution_arn}/GET/load"
-#}
+  source_arn = "${aws_api_gateway_rest_api.example.execution_arn}/GET/load"
+}
