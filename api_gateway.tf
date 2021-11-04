ec8e212a462af7a213943daa5bd5f35b14bc8ee3
Marek Toth <marektoth199@gmail.com>
2021-11-04 13:29:16 +0100

    testing new terraform v1.00

--- api_gateway.tf
+++ api_gateway.tf
@@ -25,8 +25,7 @@ resource "aws_api_gateway_integration" "lambda2" {
 
 resource "aws_api_gateway_deployment" "example" {
   depends_on = [
-    "aws_api_gateway_integration.lambda",
-    "aws_api_gateway_integration.lambda_root",
+    "aws_api_gateway_integration.lambda"
   ]
 
   rest_api_id = "${aws_api_gateway_rest_api.example.id}"
