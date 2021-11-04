bda57104d4269298f0dc7f37ef9ec426c30caa5f
TomasVank <72217029+TomasVank@users.noreply.github.com>
2021-11-04 14:40:27 +0100

    Update fakePeople.py

--- back-end/src/fake_data_generator/fakePeople.py
+++ back-end/src/fake_data_generator/fakePeople.py
@@ -5,29 +5,29 @@ import random
 import datetime
 
 #%%
-def create_rows(listOfPeoples,CodeLeaveReason,num=1):
+def create_rows(list_of_peoples,code_leave_reason,num=1):
     output = [{
-                "PersonNumber":random.choice(listOfPeoples),
-                "CodeLeaveReason":random.choice(CodeLeaveReason),
+                "PersonNumber":random.choice(list_of_peoples),
+                "CodeLeaveReason":random.choice(code_leave_reason),
                 } for _ in range(num)]
     return output
 #%%
-def DropOverLapingDatesOftheSamePerson(df):
-    uniPerson = df['PersonNumber'].unique()
+def drop_overlaping_dates_of_the_same_person(df):
+    uni_person = df['PersonNumber'].unique()
     skip = 0
-    for person in uniPerson:
-        numberOfLeaves = df["PersonNumber"].where(df["PersonNumber"]==person).count()
+    for person in uni_person:
+        number_of_leaves = df["PersonNumber"].where(df["PersonNumber"]==person).count()
         iteration = 0
-        for x in range(numberOfLeaves-1):
+        for x in range(number_of_leaves-1):
             y = 0
-            while numberOfLeaves-1 - iteration > y:
+            while number_of_leaves-1 - iteration > y:
                 i1 = pd.Interval(df['StartDate'][x+skip], df['EndDate'][x+skip])
                 i2 = pd.Interval(df['StartDate'][x +y +1+skip], df['EndDate'][x +y+1+skip])
                 overlap = i1.overlaps(i2)
                 if overlap == True:
                     df.drop(index= x + y+1+skip,axis=0, inplace=True)
                     df = df.reset_index(drop=True)
-                    numberOfLeaves = df["PersonNumber"].where(df["PersonNumber"]==person).count()
+                    number_of_leaves = df["PersonNumber"].where(df["PersonNumber"]==person).count()
                     y = y -1
                 y = y +1
             iteration = iteration +1
@@ -36,92 +36,92 @@ def DropOverLapingDatesOftheSamePerson(df):
     return df
 
 #%%
-def generateDataOfPeople(df,dfOfPeoples,LeaveTypeCode):
+def generate_data_of_people(df,df_of_peoples,leave_type_code):
     fake = Faker()
 
-    listOfLeaveTypeCode = []
-    listOfPersons = []
-    listOfSequenceNumbers = []
-    listOfJobsIndexes = []
-    listOfleaveYear = []
-    listOfStartDates = []
-    listOfEndDates = []
+    list_Of_leave_type_code = []
+    list_of_persons = []
+    list_of_sequence_numbers = []
+    list_of_jobs = []
+    list_of_years = []
+    list_of_start_dates = []
+    list_of_end_dates = []
 
     for x in range (df.shape[0]):
-        listOfPersons.append(df['PersonNumber'][x])
-        sequnceNumber = listOfPersons.count(df['PersonNumber'][x])
-        listOfSequenceNumbers.append(sequnceNumber)
-        idxOfJob = dfOfPeoples.index[dfOfPeoples['PersonNumber'] == df['PersonNumber'][x]].tolist()
-        listOfJobsIndexes.append(dfOfPeoples['EmploymentNumber'][idxOfJob[0]])
+        list_of_persons.append(df['PersonNumber'][x])
+        sequnceNumber = list_of_persons.count(df['PersonNumber'][x])
+        list_of_sequence_numbers.append(sequnceNumber)
+        idx_of_job = df_of_peoples.index[df_of_peoples['PersonNumber'] == df['PersonNumber'][x]].tolist()
+        list_of_jobs.append(df_of_peoples['EmploymentNumber'][idx_of_job[0]])
 
         if sequnceNumber == 1:
-            listOfLeaveTypeCode.append(random.choice(LeaveTypeCode))
-            newIntervalStartDate = fake.date_between(start_date='-600d', end_date='today')
-            newIntervaEndDate = (pd.to_datetime(newIntervalStartDate) + datetime.timedelta(days=10)).date()
+            list_Of_leave_type_code.append(random.choice(leave_type_code))
+            new_interval_start_date = fake.date_between(start_date='-600d', end_date='today')
+            new_interval_enddate = (pd.to_datetime(new_interval_start_date) + datetime.timedelta(days=10)).date()
 
-            listOfStartDates.append(newIntervalStartDate)
-            listOfEndDates.append(fake.date_between(start_date=newIntervalStartDate, end_date=newIntervaEndDate))
-            listOfleaveYear.append(newIntervalStartDate.year)
+            list_of_start_dates.append(new_interval_start_date)
+            list_of_end_dates.append(fake.date_between(start_date=new_interval_start_date, end_date=new_interval_enddate))
+            list_of_years.append(new_interval_start_date.year)
 
         else:
-            idx = listOfPersons.index(df['PersonNumber'][x])
-            newIntervalStartDate = fake.date_between(start_date='-100d', end_date='today')
-            newIntervaEndDate = (pd.to_datetime(newIntervalStartDate) + datetime.timedelta(days=10)).date()
+            idx = list_of_persons.index(df['PersonNumber'][x])
+            new_interval_start_date = fake.date_between(start_date='-100d', end_date='today')
+            new_interval_enddate = (pd.to_datetime(new_interval_start_date) + datetime.timedelta(days=10)).date()
 
-            listOfStartDates.append(newIntervalStartDate)
-            listOfEndDates.append(fake.date_between(start_date=newIntervalStartDate, end_date=newIntervaEndDate))
-            listOfLeaveTypeCode.append(listOfLeaveTypeCode[idx])
-            listOfleaveYear.append(newIntervalStartDate.year)
+            list_of_start_dates.append(new_interval_start_date)
+            list_of_end_dates.append(fake.date_between(start_date=new_interval_start_date, end_date=new_interval_enddate))
+            list_Of_leave_type_code.append(list_Of_leave_type_code[idx])
+            list_of_years.append(new_interval_start_date.year)
 
-    df['SequenceNumber'] = listOfSequenceNumbers
-    df['EmploymentNumber'] = listOfJobsIndexes
-    df['LeaveYear'] = listOfleaveYear
-    df['LeaveTypeCode'] = listOfLeaveTypeCode
-    df['StartDate'] = listOfStartDates
-    df['EndDate'] = listOfEndDates
+    df['SequenceNumber'] = list_of_sequence_numbers
+    df['EmploymentNumber'] = list_of_jobs
+    df['LeaveYear'] = list_of_years
+    df['leave_type_code'] = list_Of_leave_type_code
+    df['StartDate'] = list_of_start_dates
+    df['EndDate'] = list_of_end_dates
     return df
 #%%
-def resetSequenceNumber(df):
-    listOfPersons = []
-    listOfSequenceNumbers = []
+def reset_sequence_number(df):
+    list_of_persons = []
+    list_of_sequence_numbers = []
 
     df.drop(['SequenceNumber'], axis=1, inplace=True)
     for x in range (df.shape[0]):
-        listOfPersons.append(df['PersonNumber'][x])
-        sequnceNumber =listOfPersons.count(df['PersonNumber'][x])
-        listOfSequenceNumbers.append(sequnceNumber)
+        list_of_persons.append(df['PersonNumber'][x])
+        sequnceNumber =list_of_persons.count(df['PersonNumber'][x])
+        list_of_sequence_numbers.append(sequnceNumber)
 
-    df['SequenceNumber'] = listOfSequenceNumbers
+    df['SequenceNumber'] = list_of_sequence_numbers
     return df
 
 #%%
-LeaveTypeCode = ("WET", "BOV")
-CodeLeaveReason = ("HOL", "Health","")
+leave_type_code = ("WET", "BOV")
+code_leave_reason = ("HOL", "Health","")
 #%%
-dfOfPeoples = pd.read_csv("databaseOfPeople.csv")
-dfOfPeoples = dfOfPeoples.sort_values("PersonNumber")
-dfOfPeoples = dfOfPeoples.reset_index(drop=True)
-listOfPeoples = dfOfPeoples["PersonNumber"].tolist()
+df_of_peoples = pd.read_csv("databaseOfPeople.csv")
+df_of_peoples = df_of_peoples.sort_values("PersonNumber")
+df_of_peoples = df_of_peoples.reset_index(drop=True)
+list_of_peoples = df_of_peoples["PersonNumber"].tolist()
 #%%
-df = pd.DataFrame(create_rows(listOfPeoples,CodeLeaveReason,5))
+df = pd.DataFrame(create_rows(list_of_peoples,code_leave_reason,5))
 
 df = df.sort_values("PersonNumber")
 df = df.reset_index(drop=True)
 #%%
-df = generateDataOfPeople(df,dfOfPeoples,LeaveTypeCode)
+df = generate_data_of_people(df,df_of_peoples,leave_type_code)
 
 #%%
 df['StartDate'] = pd.to_datetime(df['StartDate'])
 df['EndDate'] = pd.to_datetime(df['EndDate'])
 
-df = DropOverLapingDatesOftheSamePerson(df)
+df = drop_overlaping_dates_of_the_same_person(df)
 #%%
 df = df.sort_values(['PersonNumber', 'StartDate'])
 df = df.reset_index(drop=True)
 #%%
-df = resetSequenceNumber(df)
+df = reset_sequence_number(df)
 #%%
-df = df[['PersonNumber', 'EmploymentNumber', 'SequenceNumber', 'LeaveYear', 'LeaveTypeCode', 'StartDate', 'EndDate', 'CodeLeaveReason']]
+df = df[['PersonNumber', 'EmploymentNumber', 'SequenceNumber', 'LeaveYear', 'leave_type_code', 'StartDate', 'EndDate', 'CodeLeaveReason']]
 #%%
 df['StartDate'] = df["StartDate"].dt.strftime("%d/%m/%Y")
 df['EndDate'] = df["EndDate"].dt.strftime("%d/%m/%Y")
