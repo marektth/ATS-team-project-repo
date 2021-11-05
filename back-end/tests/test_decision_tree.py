aadbb01beed21f2ec9adac23ab75747604b10074
ludex15 <ludex15@me.com>
2021-11-05 14:23:36 +0100

    added init files for packaging

--- back-end/tests/test_decision_tree.py
+++ back-end/tests/test_decision_tree.py
@@ -2,17 +2,27 @@
 import unittest
 import pandas as pd
 from pandas.util.testing import assert_frame_equal # <-- for testing dataframes
-import decision_tree as dt
+
+import src.decision_tree_pkg as dt
+
+
 
 class TestDecisiontree(unittest.TestCase):
     def test_status_column_add(self):
-        dum = pd.read_csv("dummy_df.csv")
-        right = pd.read_csv("dummy_df_1.csv")
-        test = dt.status_column_add(dum)
-        assert_frame_equal(test, right)
 
-    # def test_two(self):
-    #     x = "hello"
-    #     assert hasattr(x, "check")
+        input_df = {'': [0, 1],
+                     "Status": ["Accepted", "Rejected"]}
+        input_df = pd.DataFrame(input_df)
+
+        output_df = {'': [0, 1],
+                    "Status": [1, 0],
+                    "Overlap": [0,0]}
+
+        output_df = pd.DataFrame(output_df)
+
+        df_to_test = dt.status_column_add(input_df)
+        assert_frame_equal(df_to_test, output_df)
+
 if __name__ == '__main__':
     unittest.main()
+
