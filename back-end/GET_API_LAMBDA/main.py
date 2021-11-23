08cc490fee8c466e83a407187079251338705f01
unknown <marektoth199@gmail.com>
2021-11-23 15:43:36 +0100

    commited backend logic changes

--- back-end/GET_API_LAMBDA/main.py
+++ back-end/GET_API_LAMBDA/main.py
@@ -3,18 +3,31 @@ from pprint import pprint
 import boto3
 from botocore.exceptions import ClientError
 import os
+import pandas as pd
 
 def lambda_handler(event, context):
     # TODO implement
     
+    #print(event)
+    
+    employeeID = event["queryStringParameters"]['personID']
     
     dynamodb = boto3.resource('dynamodb')
     table_name = os.environ['TABLE_NAME']
     table = dynamodb.Table(table_name)
-
+    list_to_return = []
     try:
         x = table.scan()
-        print(x)
+        #print(x)
+        df = pd.json_normalize(x['Items'])
+        #print(df['EmployeeID'])
+        return_data = df.index[df['Employee ID'] == employeeID].tolist()
+        #list_to_return.append(df['EmployeeID'][return_data][0])
+        list_to_return = df.iloc[return_data]
+        list_to_return = df.to_json(orient="split")
+        parsed = json.loads(list_to_return)
+        print(parsed)
+        
     except ClientError as e:
         print()
         response = {
@@ -36,7 +49,7 @@ def lambda_handler(event, context):
             'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
         },
-        "body": json.dumps(x['Items'])
+        "body": json.dumps(parsed)
         }
         
         return response
--- back-end/POST_API_LAMBDA/main.py
+++ back-end/POST_API_LAMBDA/main.py
@@ -16,25 +16,21 @@ def lambda_handler(event, context):
     line_to_add = json.loads(event['body'])
     print(line_to_add)
        
-    actual_date = json.dumps(line_to_add['dateOfRequest'])
-    person_num = json.dumps(line_to_add['Person_Number'])
-    date_time = now.strftime("%m/%d/%Y, %H:%M:%S")+"_"+person_num
-    code_leave_reason = json.dumps(line_to_add['codeLeaveReason'])
-    start_date = json.dumps(line_to_add['startDate'])
-    end_date = json.dumps(line_to_add['endDate'])
-    leave_reason = json.dumps(line_to_add['leaveReason'])
-    status = json.dumps(line_to_add['status'])
+    vacation_date = json.dumps(line_to_add['Vacation Date'])
+    EmployeeID = json.dumps(line_to_add['Employee ID'])
+    date_time = now.strftime("%m/%d/%Y, %H:%M:%S")+"_"+EmployeeID
+    code_leave_reason = json.dumps(line_to_add['Code Leave Reason'])
+    leave_reason = json.dumps(line_to_add['Leave Reason'])
+    status = json.dumps(line_to_add['Status'])
     table = dynamodb_res.Table(table_name)
         
     item = {
         "id": date_time,
-        "dateOfRequest": actual_date,
-        "Person_Number": person_num,
-        "startDate": start_date,
-        "endDate": end_date,
-        "codeLeaveReason": code_leave_reason,
-        "leaveReason": leave_reason,
-        "status": status
+        "Vacation Date": vacation_date,
+        "Employee ID": EmployeeID,
+        "Code Leave Reason": code_leave_reason,
+        "Leave Reason": leave_reason,
+        "Status": status
     }
         
     table.put_item(Item=item)
