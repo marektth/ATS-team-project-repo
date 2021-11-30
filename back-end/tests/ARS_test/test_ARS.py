01f793190dc735397ccef1fd53a60c569b01867f
unknown <daniel.hreha@gmail.com>
2021-11-30 14:35:32 +0100

    11 Tests created

--- back-end/tests/ARS_test/test_ARS.py
+++ back-end/tests/ARS_test/test_ARS.py
@@ -1,8 +1,9 @@
 import unittest
-import pandas as pd
-from src.deciding_algorithm.ARS import ARS
-from pandas.util.testing import assert_frame_equal
+
 import numpy as np
+import pandas as pd
+from pandas.util.testing import assert_frame_equal
+from src.deciding_algorithm.ARS import ARS
 
 
 class TestARS(unittest.TestCase):
@@ -25,10 +26,6 @@ class TestARS(unittest.TestCase):
         self.request_pending = pd.DataFrame(self.request,index=[1])
         self.request_pending = self.request_pending.reset_index(drop=True)
         self.request_series = pd.Series(self.request)
-
-        
-
-
 
     def test_get_requests(self):  
         output = self.ars._ARS__get_requests(status = "Pending")
@@ -59,24 +56,47 @@ class TestARS(unittest.TestCase):
 
     def test_get_ou_employees(self):
         input_id = self.ars._ARS__get_ou_employees(self.request_series)
-        print(type(input_id))
         input_id = input_id.tolist()
         output = [79, 69, 62, 54, 96]
-        print(type(output))
-
         self.assertEqual(input_id, output)
 
     def test_get_size_of_ou(self):
-        pass
+        input_size = self.ars._ARS__get_size_of_ou(self.request_series)
+        self.assertEqual(input_size, 5)
 
     def test_get_ou_absence_data(self):
-        pass
+        input_data = self.ars._ARS__get_ou_absence_data(self.request_series)
+        output = {
+            "id": 0,
+            "EmployeeID": 54,
+            "DateOfAbsence": "23/10/2021",
+            "Status": "Accepted",
+            "Rating": 1
+        }
+        output_data = pd.DataFrame(output, index=[0])
+        assert_frame_equal(output_data.reset_index(drop=True), input_data.reset_index(drop=True))
 
     def test_get_ou_same_job_employees(self):
-        pass
+        input_same_job = self.ars._ARS__get_ou_same_job_employees(self.request_series)
+        data = {'EmployeeID':[54, 96],
+                'EmployeeName':['axel style', 'jax barker'],
+                'EmploymentNumber':[0, 0],
+                'OUID':[7, 7]}
+        
+        output_same_job = pd.DataFrame(data)
+        assert_frame_equal(output_same_job.reset_index(drop=True), input_same_job.reset_index(drop=True))
 
     def test_get_ou_same_job_absence(self):
-        pass
+        input_same_job_absence = self.ars._ARS__get_ou_same_job_absence(self.request_series)
+        data = {
+            "id": 0,
+            "EmployeeID": 54,
+            "DateOfAbsence": "23/10/2021",
+            "Status": "Accepted",
+            "Rating": 1
+        }
+        output_same_job_absence = pd.DataFrame(data, index=[0])
+        assert_frame_equal(output_same_job_absence.reset_index(drop=True), input_same_job_absence.reset_index(drop=True))
 
     def test_convert_to_dayofyear(self):
         request = self.ars._ARS__convert_to_dayofyear(self.request_pending,
