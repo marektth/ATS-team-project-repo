ee90a02b72fb4a5ef08f5148a308ef8ea63486b3
unknown <marektoth199@gmail.com>
2021-12-03 12:45:31 +0100

    fixing resource naming in IaaC

--- api_gateway.tf
+++ api_gateway.tf
@@ -25,7 +25,7 @@ resource "aws_api_gateway_integration" "lambda2" {
 
 resource "aws_api_gateway_integration" "lambda3" {
   rest_api_id = "${aws_api_gateway_rest_api.example.id}"
-  resource_id = "${aws_api_gateway_method.get_ou_method.resource_id}"
+  resource_id = "${aws_api_gateway_method.get_ou.resource_id}"
   http_method = "${aws_api_gateway_method.get.http_method}"
 
   integration_http_method = "GET"
