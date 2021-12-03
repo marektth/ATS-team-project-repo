0abd83f9057c69991cd2e9e2b4db91f4296ec544
unknown <marektoth199@gmail.com>
2021-12-03 12:51:21 +0100

    test

--- api_gateway.tf
+++ api_gateway.tf
@@ -26,7 +26,7 @@ resource "aws_api_gateway_integration" "lambda2" {
 resource "aws_api_gateway_integration" "lambda3" {
   rest_api_id = "${aws_api_gateway_rest_api.example.id}"
   resource_id = "${aws_api_gateway_method.get_ou.resource_id}"
-  http_method = "${aws_api_gateway_method.get.http_method}"
+  http_method = "${aws_api_gateway_method.get_ou.http_method}"
 
   integration_http_method = "GET"
   type                    = "AWS_PROXY"
