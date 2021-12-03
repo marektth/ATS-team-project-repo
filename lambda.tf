ee8ac19d1b3254e3e6237008576db4d51838e3e9
unknown <marektoth199@gmail.com>
2021-12-03 13:01:46 +0100

    test v1.2

--- lambda.tf
+++ lambda.tf
@@ -141,7 +141,7 @@ resource "aws_api_gateway_resource" "get" {
 resource "aws_api_gateway_resource" "get_ou" {
   rest_api_id = "${aws_api_gateway_rest_api.example.id}"
   parent_id   = "${aws_api_gateway_rest_api.example.root_resource_id}"
-  path_part   = "load_team_absence_data"
+  path_part   = "load_team_absence"
 }
 
 resource "aws_api_gateway_method" "post" {
