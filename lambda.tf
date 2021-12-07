75c1471260874475652efc7f580fba0ccec9cfe5
unknown <marektoth199@gmail.com>
2021-12-07 10:13:54 +0100

    deploying sulution v1.1

--- lambda.tf
+++ lambda.tf
@@ -16,17 +16,6 @@ terraform{
   }
 }
 
-resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
-  name = "terraform-state-lock-dynamo"
-  hash_key = "LockID"
-  read_capacity = 20
-  write_capacity = 20
- 
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
 
 resource "aws_lambda_function" "manager_lambda" {
   function_name = "MANAGER_LAMBDA"
