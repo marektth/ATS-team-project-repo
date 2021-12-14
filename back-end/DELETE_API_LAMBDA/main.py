68e9f6aa60abe62ab4f1fcbce824e8b115ca8ef3
unknown <marektoth199@gmail.com>
2021-12-14 20:32:14 +0100

    new commits

--- back-end/DELETE_API_LAMBDA/main.py
+++ back-end/DELETE_API_LAMBDA/main.py
@@ -2,6 +2,7 @@ import json
 import boto3
 import os
 import boto3
+import pandas as pd
 from botocore.exceptions import ClientError
 from datetime import datetime
 
@@ -9,10 +10,16 @@ s3_c = boto3.client('s3')
 
 s3_bucket_name = os.environ.get('BUCKET_NAME')
 s3_key_website = os.environ.get('OBJECT_NAME')
+s3_key_website_employees = os.environ.get('OBJECT_NAME_EMPLOYEES')
 
 
-def lambda_handler(event, context):
+def load_table(s3_bucket_name, object_key):
+    resp=s3_c.get_object(Bucket=s3_bucket_name, Key=object_key)
+    data=resp.get('Body')
+    return pd.read_json(data)
+    
 
+def permission_testing(s3_bucket_name, s3_key_website):
     s3 = boto3.resource('s3')
     
     try:
@@ -24,35 +31,127 @@ def lambda_handler(event, context):
         else:
             print(f"File ({s3_key_website}) required by this function is not accessible!")
             raise e
- 
-    line_to_add = json.loads(event['body'])
-    
-    table_id = line_to_add['id']
-   
-    resp=s3_c.get_object(Bucket=s3_bucket_name, Key=s3_key_website)
-    data=resp.get('Body')
-    
-    json_data = json.load(data)
-    
-    for i in range(len(json_data)):
-        if json_data[i]["id"] == table_id:
-            json_data.pop(i)
-            break
-    
-   
-    s3_c.put_object(Bucket=s3_bucket_name, Key=s3_key_website, Body=json.dumps(json_data).encode('UTF-8'))
-     
-    response = {
-        "statusCode": 200,
-        "headers": {
-            'Access-Control-Allow-Headers': 'Content-Type',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-        },
-        "body": json.dumps("WRITTEN")
-    }
 
-    return response
+def response_flag(return_status, s3_bucket, s3_object_absence, s3_object_employees, absence_data_param, employees_data_param):
+    
+    if(return_status == "OK"):
+       
+        
+        if absence_data_param is not None:
+            result = absence_data_param.to_json(orient="records")
+            parsed = json.loads(result)
+            s3_c.put_object(Bucket=s3_bucket, Key=s3_object_absence, Body=json.dumps(parsed,indent=4).encode('UTF-8'))
+        if employees_data_param is not None:
+            result = employees_data_param.to_json(orient="records")
+            parsed_employees = json.loads(result)
+            s3_c.put_object(Bucket=s3_bucket, Key=s3_object_employees, Body=json.dumps(parsed_employees,indent=4).encode('UTF-8'))
+        
+        response = {
+            "statusCode": 200,
+            "headers": {
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'OPTIONS,POST,GET,DELETE'
+            },
+            "body": json.dumps("Success, Written !")
+        }
+        return response
+    elif(return_status == "NOA"):
+        response = {
+            "statusCode": 405,
+            "headers": {
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'OPTIONS,POST,GET,DELETE'
+            },
+            "body": json.dumps("Not allowed !")
+        }
+        return response
+    else:
+        response = {
+            "statusCode": 404,
+            "headers": {
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'OPTIONS,POST,GET,DELETE'
+            },
+            "body": json.dumps("Error !")
+        }
+        return response
+        
+
+
+def lambda_handler(event, context):
+
+    s3 = boto3.resource('s3')
+    permission_testing(s3_bucket_name,s3_key_website)
+
+    absence_data = load_table(s3_bucket_name, object_key=s3_key_website)
+    employees_data = load_table(s3_bucket_name, object_key=s3_key_website_employees)
+    
+    
+    
+    line_to_add = json.loads(event['body'])
+    request_id_to_cancel = line_to_add['id']
+   
+    date_format = '%d/%m/%Y'
+    cancel_date = datetime.today().strftime(date_format)
+    
+     # get absence request
+    request_to_cancel = (absence_data.loc[absence_data["id"] == request_id_to_cancel]).iloc[0]
+
+    # check if request to cancel is accepted or pending
+    if request_to_cancel['Status'] == "Pending":
+        # if pending change status to cancelled 
+        # do not change column AbsenceTo
+        absence_data.loc[absence_data['id'] == request_id_to_cancel, 'Status'] = "Cancelled"
+        absence_data.loc[absence_data['id'] == request_id_to_cancel, 'StatusResolution'] = "Cancelled Pending request"
+        return response_flag("OK", s3_bucket = s3_bucket_name, s3_object_absence=s3_key_website, s3_object_employees=None, absence_data_param = absence_data, employees_data_param=None)
+
+    elif request_to_cancel['Status'] == "Accepted":
+        # if accepted change status to cancelled
+        # change column AbcenceTo to cancel_date
+        # check if not cancel_date is not past AbsenceTo
+        if pd.to_datetime(request_to_cancel['AbsenceTo'], format=date_format) > pd.to_datetime(cancel_date, format=date_format):
+
+            #check if request if TIMEOFF
+            did_timeoff = False
+            if request_to_cancel['AbsenceTypeCode'] == "TIM":
+                did_timeoff = True
+                # add remaining absence hours to leave_balance of employee
+                absence_from = pd.to_datetime(cancel_date, format=date_format)
+                absence_to = pd.to_datetime(request_to_cancel["AbsenceTo"], format=date_format)
+                working_hours = 8
+                remaining_hours = ((absence_to - absence_from).days)*working_hours
+                employee_leave_balance = (employees_data.loc[employees_data['EmployeeID']== request_to_cancel['EmployeeID']]['LeaveBalance']).values[0]
+                employees_data.loc[employees_data['EmployeeID'] == request_to_cancel['EmployeeID'], 'LeaveBalance'] = employee_leave_balance + remaining_hours
+
+            # set status to cancelled, change AbsenceTo to cancel_date, set StatusResolution to display user request state
+            absence_data.loc[absence_data['id'] == request_id_to_cancel, 'Status'] = "Cancelled"
+            absence_data.loc[absence_data['id'] == request_id_to_cancel, 'AbsenceTo'] = cancel_date
+            absence_data.loc[absence_data['id'] == request_id_to_cancel, 'StatusResolution'] = "Cancelled Accepted request"
+            if did_timeoff:
+                return response_flag("OK", s3_bucket = s3_bucket_name, s3_object_absence=s3_key_website, s3_object_employees=s3_key_website_employees, absence_data_param=absence_data, employees_data_param=employees_data)
+            else:
+                return response_flag("OK", s3_bucket = s3_bucket_name, s3_object_absence=s3_key_website, s3_object_employees=None, absence_data_param = absence_data, employees_data_param=None)
+        else:
+            # if canceling is later or same as the end of absence
+            return response_flag("NOA", s3_bucket = s3_bucket_name, s3_object_absence=s3_key_website, s3_object_employees=s3_key_website_employees, absence_data_param=absence_data, employees_data_param=employees_data)
+    else:
+        # response - cancelling not allowed on other types of statuses (Rejected, ...)
+        return response_flag("NOA", s3_bucket = s3_bucket_name, s3_object_absence=s3_key_website, s3_object_employees=s3_key_website_employees, absence_data_param=absence_data, employees_data_param=employees_data)
+        
+
+    # if response OK, here save tables to S3
+    # return response_flag("OK", s3_bucket = s3_bucket_name, s3_object_absence=s3_key_website, s3_object_employees=s3_key_website_employees, absence_data_param=absence_data, employees_data_param=employees_data)
+   
+   
+    
+    
+    
+    
+     
+    
     
   
     
--- back-end/GET_API_LAMBDA/main.py
+++ back-end/GET_API_LAMBDA/main.py
@@ -8,6 +8,7 @@ s3 = boto3.client('s3')
 
 s3_bucket_name = os.environ.get('BUCKET_NAME')
 s3_key_website = os.environ.get('OBJECT_NAME')
+s3_key_website_employee = os.environ.get('OBJECT_NAME_EMPLOYEE')
 
 
 def lambda_handler(event, context):
@@ -30,30 +31,48 @@ def lambda_handler(event, context):
         obj = s3.Object(s3_bucket_name, s3_key_website)
         data = obj.get()['Body'].read().decode('utf-8')
         return_data = []
+        return_data_employee = []
        
         if employee_id == "all":
             json_data = json.loads("" + 
             data.replace("}\n{", "},\n{") + 
             "")
             return_data.append(json_data)
-            
-        
-        
-        
+   
         json_data = json.loads("" + 
             data.replace("}\n{", "},\n{") + 
             "")
-    
+        
+        
+        obj_employee = s3.Object(s3_bucket_name, s3_key_website_employee)
+        data_employee = obj_employee.get()['Body'].read().decode('utf-8')
+        return_data_employee = []
+        
+        if employee_id == "all":
+            json_data_employee = json.loads("" + 
+            data_employee.replace("}\n{", "},\n{") + 
+            "")
+            return_data_employee.append(json_data_employee)
+   
+        json_data_employee = json.loads("" + 
+            data_employee.replace("}\n{", "},\n{") + 
+            "")
+        
+        
       
         for index in range (len(json_data)):
             if str(json_data[index]['EmployeeID']) == employee_id:
                 json_data[index]['EmployeeID'] = employee_id
                 return_data.append(json_data[index])
-                
-      
-        parsed = json.dumps(return_data)
-     
         
+    
+        return_data_employee.append(next((item for item in json_data_employee if str(item["EmployeeID"]) == employee_id),None))
+        
+        parsed = {
+          "AbsenceData" : return_data,
+          "EmployeeData" : return_data_employee
+        }
+  
     except ClientError as e:
         print()
         response = {
@@ -75,7 +94,7 @@ def lambda_handler(event, context):
             'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
         },
-        "body": parsed
+        "body": json.dumps(parsed)
         }
         
         return response
