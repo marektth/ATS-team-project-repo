5c7f8b5952bfad9a0c96b4440051be5902d4d99d
unknown <marektoth199@gmail.com>
2022-01-10 10:29:50 +0100

    updated IaaC - fixed existing stage

--- api_gateway.tf
+++ api_gateway.tf
@@ -56,7 +56,7 @@ resource "aws_api_gateway_deployment" "example" {
 resource "aws_api_gateway_stage" "development" {
   deployment_id = aws_api_gateway_deployment.example.id
   rest_api_id   = aws_api_gateway_rest_api.example.id
-  stage_name    = "leaveRequest"
+  stage_name    = "leaveRequestTF"
 }
 
 resource "aws_api_gateway_usage_plan" "api-plan" {
