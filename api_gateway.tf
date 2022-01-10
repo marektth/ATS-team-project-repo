1606355e8b1de21803e48b581fe8a35bffed1c69
unknown <marektoth199@gmail.com>
2022-01-10 12:06:59 +0100

    Testing IaaC CORS

--- api_gateway.tf
+++ api_gateway.tf
@@ -226,6 +226,15 @@ module "cors" {
   allow_credentials = true
 }
 
+module "cors" {
+  source  = "squidfunk/api-gateway-enable-cors/aws"
+  version = "0.3.3"
+
+  api_id            = aws_api_gateway_rest_api.example.id
+  api_resource_id   = aws_api_gateway_resource.post.id
+  allow_credentials = true
+}
+
 output "base_url" {
   value = "${aws_api_gateway_deployment.example.invoke_url}"
 }
