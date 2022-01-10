60c8b0d3fd32b0f26d6de85bd8467dd7c6f1a0a6
unknown <marektoth199@gmail.com>
2022-01-10 11:00:15 +0100

    Testing IaaC CORS - fixed typos

--- api_gateway.tf
+++ api_gateway.tf
@@ -166,9 +166,9 @@ resource "aws_api_gateway_deployment" "example" {
   depends_on = [
     "aws_api_gateway_integration.lambda",
     "aws_api_gateway_method_response.post_api_response",
-    "aws_api_gateway_method_response.get_response",
-    "aws_api_gateway_method_response.get_ou_response",
-    "aws_api_gateway_method_response.delete_response"
+    "aws_api_gateway_method_response.get_api_response",
+    "aws_api_gateway_method_response.get_ou_api_response",
+    "aws_api_gateway_method_response.delete_api_response"
   ]
 
   rest_api_id = "${aws_api_gateway_rest_api.example.id}"
